4e67ab0c4c2939aa88ca2003211e6047acd3b41c
Anastasia-front <palitsanastasia3.ap@gmail.com>
2023-12-21 15:27:38 +0200

    refactor : add adaptive styles to Activities slider

--- src/components/Activities.tsx
+++ src/components/Activities.tsx
@@ -6,8 +6,8 @@ import { Subtitle, Title } from ".";
 
 import a7 from "../assets/jpg/content/activities//mountains.jpg";
 import a1 from "../assets/jpg/content/activities/bays.jpg";
-import a2 from "../assets/jpg/content/activities/fjords.jpg";
 import a3 from "../assets/jpg/content/activities/coasts.jpg";
+import a2 from "../assets/jpg/content/activities/fjords.jpg";
 import a4 from "../assets/jpg/content/activities/hiking.jpg";
 import a5 from "../assets/jpg/content/activities/kayaking.jpg";
 import a6 from "../assets/jpg/content/activities/local-settlements.jpg";
@@ -15,6 +15,7 @@ import a8 from "../assets/jpg/content/activities/northern-lights.jpg";
 import a9 from "../assets/jpg/content/activities/sailing.jpg";
 import a10 from "../assets/jpg/content/activities/skiing.jpg";
 import { getAltNameFromPath } from "../helpers";
+import { useScreenQuery } from "../hooks";
 
 const activities = [
   { img: a1, number: 424 },
@@ -29,12 +30,22 @@ const activities = [
   { img: a10, number: 767 },
 ];
 export function Activities() {
+  const { isScreenMobileLg, isScreenTabletSm, isScreenTabletLg } =
+    useScreenQuery();
+
   const slidesRef = useRef<HTMLUListElement | null>(null);
   const [startIndex, setStartIndex] = useState(0);
 
+  const visibleSlides = isScreenTabletLg
+    ? 4
+    : isScreenTabletSm
+    ? 3
+    : isScreenMobileLg
+    ? 2
+    : 1;
+
   function showSlide(direction: "next" | "prev"): void {
     const totalSlides = activities.length;
-    const visibleSlides = 4;
 
     let newStartIndex = startIndex;
 
@@ -55,7 +66,7 @@ export function Activities() {
   }
 
   const isPrevButtonDisabled = startIndex === 0;
-  const isNextButtonDisabled = startIndex + 4 >= activities.length;
+  const isNextButtonDisabled = startIndex + visibleSlides >= activities.length;
 
   return (
     <section id="Activities" className="container">
@@ -71,14 +82,18 @@ export function Activities() {
           Prev
         </button>
         <ul className="slider-content">
-          {/* {activities.slice(0, visibleSlides).map((activity) => ( */}
-          {activities.slice(startIndex, startIndex + 4).map((activity) => (
-            <li key={getAltNameFromPath(activity.img)}>
-              <img src={activity.img} alt={getAltNameFromPath(activity.img)} />
-              <p>{getAltNameFromPath(activity.img)}</p>
-              <span>{activity.number} activities</span>
-            </li>
-          ))}
+          {activities
+            .slice(startIndex, startIndex + visibleSlides)
+            .map((activity) => (
+              <li key={getAltNameFromPath(activity.img)}>
+                <img
+                  src={activity.img}
+                  alt={getAltNameFromPath(activity.img)}
+                />
+                <p>{getAltNameFromPath(activity.img)}</p>
+                <span>{activity.number} activities</span>
+              </li>
+            ))}
         </ul>
         <button
           className="slider-button slider-button__next"
